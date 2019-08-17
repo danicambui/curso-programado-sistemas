d69bd28418fe5d31feaa1bd4092c150f5ee03e15
Daniela Cambui <cambuidani@gmail.com>
2019-08-17 11:32:27 -0300

    Estou subindo a nova linha

--- prj_salvando_no_git/Program.cs
+++ prj_salvando_no_git/Program.cs
@@ -7,6 +7,13 @@ namespace prj_salvando_no_git
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Aula de Git e Github!!");
+
+            Console.WriteLine("preparando para subir no github");
+
+            Console.WriteLine("Escreva seu nome: ");
+            Console.ReadLine();
         }
     }
 }
