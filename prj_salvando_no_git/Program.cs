35afe83f9ed0e9c5b2f8558d2ee753536828b62e
Daniela Cambui <cambuidani@gmail.com>
2019-08-17 10:28:07 -0300

    salvando o projeto_solution

---
+++ prj_salvando_no_git/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace prj_salvando_no_git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
